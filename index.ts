d9d1f0ded292ebda0961d8d3f7cdfd2758d50cc7
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-01-23 23:55:10 -0300

    inicialização do servidor

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import AsyncHandler from "./src/asyncHandler"
+import conn from "./src/conn"
+
+const app = express()
+
+app.use((req, res, next) => {
+    if (req.body) {
+        let json = JSON.stringify(req.body)
+
+        let jsonFormatado = json.replace(/\$serverTime/gi, new Date().toJSON())
+
+        req.body = JSON.parse(jsonFormatado)
+    }
+
+    next()
+})
+
+app.get("/teste", AsyncHandler(async (req, res, next) => {
+    res.json(await conn.query("SELECT NOaW() as TesteConexao"))
+}))
+
+app.listen(3001, () => {
+    console.log("API aberta: http://localhost:3001/teste")
+})
