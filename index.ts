30f8d7edbdcd861bfe23049e602186fd984762f2
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-03-16 19:00:54 -0300

    retira erro de teste e middlewere obsoleto

--- index.ts
+++ index.ts
@@ -5,20 +5,8 @@ import conn from "./src/conn"
 
 const app = express()
 
-app.use((req, res, next) => {
-    if (req.body) {
-        let json = JSON.stringify(req.body)
-
-        let jsonFormatado = json.replace(/\$serverTime/gi, new Date().toJSON())
-
-        req.body = JSON.parse(jsonFormatado)
-    }
-
-    next()
-})
-
 app.get("/teste", AsyncHandler(async (req, res, next) => {
-    res.json(await conn.query("SELECT NOaW() as TesteConexao"))
+    res.json(await conn.query("SELECT NOW() as TesteConexao"))
 }))
 
 app.use(rotas)
