1d7d9e5a7b3264309f307432c8677a6804aef447
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-05-01 17:47:03 -0300

    add função para duplicar registros parcelados

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import rotas from "./rotas"
 import AsyncHandler from "./src/asyncHandler"
 import conn from "./src/conn"
 import cors from 'cors'
+import { Registro_gasto } from "./rotas/registro_gastos"
+import moment from "moment"
 
 const app = express()
 
@@ -13,8 +15,28 @@ app.get("/teste", AsyncHandler(async (req, res, next) => {
     res.json(await conn.query("SELECT NOW() as TesteConexao"))
 }))
 
+app.post("/proximoMes/mesAtual=:mes", AsyncHandler(async (req, res) => {
+    let registrosParaDuplicar = await conn.query<Registro_gasto>("SELECT * FROM registro_gastos WHERE parcela_atual < parcelas_totais AND MONTH(data_registro) = ?", [req.params.mes])
+
+    let novosRegistros = registrosParaDuplicar.map((item) => {
+        item.parcela_atual += 1
+        item.data_registro = moment(item.data_registro).add(1, 'month').format("YYYY-MM-DD hh:mm:ss")
+        return {
+            ...item,
+            id: undefined
+        }
+    })
+
+    res.json(await conn.transaction(novosRegistros.map((item) => {
+        return {
+            query: "INSERT INTO registro_gastos SET ?",
+            params: [item]
+        }
+    })))
+}))
+
 app.use(rotas)
 
-app.listen(3001, () => {
+app.listen(3002, () => {
     console.log("API aberta: http://localhost:3001/teste")
 })
