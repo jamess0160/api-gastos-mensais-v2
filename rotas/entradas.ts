fdcb88b1f72dae0e167589c600115c5de5a47ed0
jamess0160 <tiagoribeiro12@hotmail.com.br>
2023-05-01 18:13:56 -0300

    add tipagem e filtro de mes

--- rotas/entradas.ts
+++ rotas/entradas.ts
@@ -2,18 +2,25 @@ import express from "express"
 import AsyncHandler from "../src/asyncHandler"
 import conn from "../src/conn"
 
+export type Entrada = {
+    id: number
+    tipo_id: number
+    valor: number
+    data_registro: string
+}
+
 const entradas = express()
 
 entradas.get('/entradas', AsyncHandler(async (req, res) => {
     res.json(await conn.query("SELECT * FROM entradas ORDER BY id LIMIT 500"))
 }))
 
-entradas.get('/entradas/recentes', AsyncHandler(async (req, res) => {
+entradas.get('/entradas/recentes/mes=:mes', AsyncHandler(async (req, res) => {
     res.json(await conn.query(`
         SELECT * FROM entradas WHERE id IN (
-            SELECT max(id) FROM entradas GROUP BY tipo_id
+            SELECT max(id) FROM entradas WHERE MONTH(data_registro) = ? GROUP BY tipo_id
         )
-    `))
+    `, [req.params.mes]))
 }))
 
 entradas.post('/entradas', AsyncHandler(async (req, res) => {
